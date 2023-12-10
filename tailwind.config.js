a95c05b803df95e9d8c15db858376d3348ea31e3
Durgeswarank <138798333+Durgeswarank@users.noreply.github.com>
2023-12-10 04:44:39 -0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./src/**/*.{html,ts}",
+  "./node_modules/tw-elements/dist/js/**/*.js"
+];
+export const theme = {
+  extend: {},
+};
+export const darkMode = "class";
+export const plugins = [require("tw-elements/dist/plugin.cjs")];
